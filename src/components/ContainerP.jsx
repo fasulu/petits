0a45e1a7dfd6ae55cacdc9eab7a0fc68565062de
fasulu <rfasulu@hotmail.com>
2021-04-27 12:25:46 +0200

    Style changed

--- src/components/ContainerP.jsx
+++ src/components/ContainerP.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Circle.css'
 import Circle from "./Circle"
+import '../components/ContainerP.css'
 
 class ContainerP extends Component {
 
@@ -14,36 +15,38 @@ class ContainerP extends Component {
         // console.log("render in containerP", this.props.player2Tokens)
         return (
 
-            <div className="containerP1">
-                <div className="divchildP1">
-                    <div >
-                        <Circle circleColor={this.props.player1Tokens[0] ? "green" : "black"} />
-                        <Circle circleColor={this.props.player1Tokens[1] ? "green" : "black"} />
-                    </div>
+            <div  >
 
-                    <div >
-                        <Circle circleColor={this.props.player1Tokens[2] ? "green" : "black"} />
-                        <Circle circleColor={this.props.player1Tokens[3] ? "green" : "black"} />
-                    </div>
-                </div>
-
-
-                <div className="containerP2">
-                    <div className="divchildP2">
+                <div className="flex grid-item-5">
+                    <div className="divchildP1">
                         <div >
-                            <Circle circleColor={this.props.player2Tokens[0] ? "orange" : "black"} />
-                            <Circle circleColor={this.props.player2Tokens[1] ? "orange" : "black"} />
+                            <Circle circleColor={this.props.player1Tokens[0] ? "green" : "black"} />
+                            <Circle circleColor={this.props.player1Tokens[1] ? "green" : "black"} />
                         </div>
 
                         <div >
-                            <Circle circleColor={this.props.player2Tokens[2] ? "orange" : "black"} />
-                            <Circle circleColor={this.props.player2Tokens[3] ? "orange" : "black"} />
+                            <Circle circleColor={this.props.player1Tokens[2] ? "green" : "black"} />
+                            <Circle circleColor={this.props.player1Tokens[3] ? "green" : "black"} />
+                        </div>
+                    </div>
+
+
+                    <div className="containerP2">
+                        <div className="divchildP2">
+                            <div >
+                                <Circle circleColor={this.props.player2Tokens[0] ? "orange" : "black"} />
+                                <Circle circleColor={this.props.player2Tokens[1] ? "orange" : "black"} />
+                            </div>
+
+                            <div >
+                                <Circle circleColor={this.props.player2Tokens[2] ? "orange" : "black"} />
+                                <Circle circleColor={this.props.player2Tokens[3] ? "orange" : "black"} />
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/* <i className="fas fa-circle" style={{ color: this.props.circleColor }}></i> */}
+
             </div>
-
         )
     }
 }
